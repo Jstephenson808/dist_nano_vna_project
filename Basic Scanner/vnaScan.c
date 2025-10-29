64eab8c27b100b74e950f45b5e5ebb5cc05a2b27
2884069w <2884069w@student.gla.ac.uk>
2025-10-29 18:33:35 +0000

    refactoring vnaScan.c

--- Basic Scanner/vnaScan.c
+++ Basic Scanner/vnaScan.c
@@ -1,22 +1,44 @@
+// needed for CRTSCTS macro
 #define _DEFAULT_SOURCE
 
-// C headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
 #include <inttypes.h>
 
-// Linux headers
 #include <fcntl.h>
 #include <errno.h>
 #include <termios.h>
 #include <unistd.h>
 
+#define POINTS 101
+#define MASK 135
+
+//Declaring global variables (for error handling only)
 volatile sig_atomic_t fatal_error_in_progress = 0;
 int serial_port_global = 0;
 struct termios* initial_port_settings_global = NULL;
 
+
+//Declaring structs for data points
+struct complex {
+    float re;
+    float im;
+};
+struct datapoint {
+    uint32_t frequency;
+    struct complex s11;
+    struct complex s21;
+};
+
+//=============================================
+// Port Restoration
+//=============================================
+
+/*
+ * Closes port and restores initial settings
+ */
 void close_and_reset(int serial_port, struct termios* initial_tty) {
     //printf("close and reseting");
     fflush(stdout);
@@ -29,8 +51,13 @@ void close_and_reset(int serial_port, struct termios* initial_tty) {
     close(serial_port);
 }
 
+/*
+ * Fatal error handling. 
+ * 
+ * Calls close_and_reset before allowing the program to exit normally.
+ * Uses fatal_error_in_progress to prevent infinite recursion.
+ */
 void fatal_error_signal(int sig) {
-    // avoid infinite recursion
     if (fatal_error_in_progress) {
         raise (sig);
     }
@@ -44,6 +71,20 @@ void fatal_error_signal(int sig) {
     raise (sig);
 }
 
+//=============================================
+// Serial Interface
+//=============================================
+
+/*
+ * Amend port settings
+ * 
+ * Edits port settings to interact with a serial interface.
+ * Flags should only be edited with bitwise operations.
+ * Writes are permanent: initial settings are kept to restore on program close.
+ * 
+ * @serial_port should already be opened successfully
+ * @return initial settings to restore. Also stored in global variable.
+ */
 struct termios* init_serial_settings(int serial_port) {
     struct termios *initial_tty = malloc(sizeof(struct termios)); // keep to restore settings later
     if (tcgetattr(serial_port, initial_tty) != 0) {
@@ -88,17 +129,13 @@ struct termios* init_serial_settings(int serial_port) {
     return initial_tty; // remember to restore later
 }
 
-struct complex {
-    float re;
-    float im;
-};
+//=============================================
+// Main Program
+//=============================================
 
-struct datapoint {
-    uint32_t frequency;
-    struct complex s11;
-    struct complex s21;
-};
-
+/*
+ * Issues info command and prints output
+ */
 void checkConnection(int serial_port) {
     int numBytes;
     char buffer[32];
@@ -115,12 +152,25 @@ void checkConnection(int serial_port) {
     return;
 }
 
+/*
+ * Sends NanoVNA a scan command, prints formatted output
+ * 
+ * Output is recieved in binary, in frames as specified by the datapoint struct.
+ * Output can have echoes and unexpected elements. When searching for the start of a 
+ * specified output ("skip to binary header"), uses characters to represent bytes being
+ * read and scans one byte at a time to match binary header pattern.
+ * Scanning is done with larger buffers at all other points, as 1 byte buffers are inefficient.
+ */
 int main() {
+
+    // assign error handler
 
     if (signal(SIGINT, fatal_error_signal) == SIG_ERR) {
         fprintf(stderr, "An error occurred while setting a signal handler.\n");
         return EXIT_FAILURE;
     }
+
+    // create port
 
     int serial_port = open("/dev/ttyACM0", O_RDWR);
     if (serial_port < 0) {
@@ -133,11 +183,17 @@ int main() {
 
     checkConnection(serial_port);
 
-    unsigned char msg[] = "scan 50000000 900000000 101 135\r";
-    write(serial_port, msg, sizeof(msg));
+    // give scan command
+
+    char* msg = malloc(sizeof(char)*50);    
+    snprintf(msg, sizeof(char)*50, "scan 50000000 900000000 %i %i\r", POINTS, MASK);
+    write(serial_port, msg, sizeof(char)*50);
+    free(msg);
+
+    // skip to binary header
 
     int numBytes;
-    uint16_t details[2], actual_details[2] = {135, 101};
+    uint16_t details[2], actual_details[2] = {MASK, POINTS};
     unsigned char short_buffer[4];
     unsigned char advance;
 
@@ -154,6 +210,8 @@ int main() {
 
     printf("mask: %d, points: %d\n", details[0], details[1]);
     int points = details[1];
+
+    // recieve output
 
     struct datapoint data[points];
 
