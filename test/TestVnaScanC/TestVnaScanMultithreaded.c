415c063ea6fa637cf851d69edb93ce7c5badf66d
“Keyaan <2904319s@student.gla.ac.uk>
2025-11-25 17:49:48 +0000

    Test commit, helper functions nanoVNA_avaliable and make_pipe

--- test/TestVnaScanC/TestVnaScanMultithreaded.c
+++ test/TestVnaScanC/TestVnaScanMultithreaded.c
@@ -3,6 +3,24 @@
 
 #define UNITY_INCLUDE_CONFIG_H
 
+/* Helper methods */
+
+// Will check the runtime environment for a connected NanoVNA device
+static int nanoVNA_avaliable(void) {
+    struct stat st;
+    return (stat("/dev/ttyACM0", &st) == 0);
+}
+
+// Creates a pipe helper for unit tests
+static void make_pipe(int *read_fd, int *write_fd) {
+    int fds[2];
+    TEST_ASSERT_EQUAL(0, pipe(fds));
+    *read_file_descriptor = file_descriptor[0];
+    *write_file_descriptor = file_descriptor[1];
+}
+
+
+
 void setUp(void) {
     /* This is run before EACH TEST */
 }
