788104596050ec498dad664d1470c3b47bf3f7e7
2884069w <2884069w@student.gla.ac.uk>
2025-11-26 11:20:20 +0000

    Created test prototypes for functions that require VNA emulation
    https://stgit.dcs.gla.ac.uk/team-project-h/2025/jh05/jh05-main/-/issues/73
    
    Uses TEST_IGNORE_MESSAGE to verbosely skip them, useful to ensure that
    the person compiling them knows knows what tests are not running and
    why.

--- test/TestVnaScanC/TestVnaScanMultithreaded.c
+++ test/TestVnaScanC/TestVnaScanMultithreaded.c
@@ -22,9 +22,52 @@ void test_Constants_Are_Valid() {
     TEST_ASSERT_EQUAL(135, MASK);
 }
 
+void test_configure_serial_settings_correct() {
+    TEST_IGNORE_MESSAGE("Cannot test without mocking serial connection");
+}
+
+void test_restore_serial_settings_correct() {
+    TEST_IGNORE_MESSAGE("Cannot test without mocking serial connection");
+}
+
+void test_close_and_reset_all_targets() {
+    TEST_IGNORE_MESSAGE("Cannot test without mocking restore_serial");
+}
+void test_close_and_reset_all_VNA_COUNT() {
+    TEST_IGNORE_MESSAGE("Cannot test without mocking restore_serial");
+}
+
+void test_write_command() {
+    TEST_IGNORE_MESSAGE("Cannot test without mocking serial connection");
+}
+
+void test_read_exact_reads_one_byte() {
+    TEST_IGNORE_MESSAGE("Cannot test without mocking serial connection");
+}
+
+void test_find_binary_header_handles_command_prompt() {
+    TEST_IGNORE_MESSAGE("Cannot test without mocking read()");
+}
+void test_find_binary_header_handles_random_data() {
+    TEST_IGNORE_MESSAGE("Cannot test without mocking read()");
+}
+void test_find_binary_header_fails_gracefully() {
+    TEST_IGNORE_MESSAGE("Cannot test without mocking read()");
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_Numbers_Exist);
     RUN_TEST(test_Constants_Are_Valid);
+    RUN_TEST(test_configure_serial_settings_correct);
+    RUN_TEST(test_restore_serial_settings_correct);
+    RUN_TEST(test_close_and_reset_all_targets);
+    RUN_TEST(test_close_and_reset_all_VNA_COUNT);
+    RUN_TEST(test_write_command);
+    RUN_TEST(test_read_exact_reads_one_byte);
+    RUN_TEST(test_find_binary_header_handles_command_prompt);
+    RUN_TEST(test_find_binary_header_handles_random_data);
+    RUN_TEST(test_find_binary_header_fails_gracefully);
+
     return UNITY_END();
 }
