00e600426f5f8576206c0a54a6dac98713eb0ce0
2884069w <2884069w@student.gla.ac.uk>
2025-12-20 16:55:44 +0000

    Created basic command parse script
    https://stgit.dcs.gla.ac.uk/team-project-h/2025/jh05/jh05-main/-/issues/94
    
    Can take two inputs, "scan" or "exit". Will keep looping for input until
    "exit" is entered. Will run default scan upon entering "scan".

---
+++ src/VnaScanC/VnaCommandParser.c
@@ -0,0 +1,28 @@
+#include "VnaScanMultithreaded.h"
+
+int main() {
+    // set defaults
+    long start_freq = 50000000;
+    long stop_freq = 900000000;
+    int nbr_scans = 20;
+    int sweeps = 5;
+    SweepMode sweep_mode = NUM_SWEEPS;
+    int nbr_nanoVNAs = 1;
+    const char* default_port = "/dev/ttyACM0";
+    const char **ports = (const char **)&default_port;
+
+
+    int stop = 0;
+    while (stop != 1) {
+        printf(">>> ");
+        char buff[50];
+        scanf("%s", buff);
+        if (strcmp(buff,"scan") == 0) {
+            run_multithreaded_scan(nbr_nanoVNAs, nbr_scans, start_freq, stop_freq, sweep_mode, sweeps, ports);
+        }
+        else if (strcmp(buff,"exit") == 0) {
+            stop = 1;
+        }
+    }
+    return 0;
+}
