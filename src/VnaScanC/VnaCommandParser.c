e36012dc8e33ec22c4f2da71e226526e290416e1
2884069w <2884069w@student.gla.ac.uk>
2025-12-31 15:25:36 +0000

    Seperated out main function and made arguments global variables
    https://stgit.dcs.gla.ac.uk/team-project-h/2025/jh05/jh05-main/-/issues/94
    
    Main function now seperate, just exists as an entry function to call
    readCommand. Much better style and make everything much more testable.
    Arguments now global to avoid passing around copies of them everywhere,
    as it should be very clear how they are changed (pretty much just setter
    functions).
    We will need to be careful to follow good style when it comes to
    accessing the global variables, as C does not enforce this (but we want
    to kind of pretend we're using an OO language lol)

--- src/VnaScanC/VnaCommandParser.c
+++ src/VnaScanC/VnaCommandParser.c
@@ -1,6 +1,16 @@
 #include "VnaScanMultithreaded.h"
 #include <string.h>
 
+// defaults
+long start = 50000000;
+long stop = 900000000;
+int nbr_scans = 5;
+int sweeps = 1;
+SweepMode sweep_mode = NUM_SWEEPS;
+int num_vnas = 1;
+const char* default_port = "/dev/ttyACM0";
+const char **ports = (const char **)&default_port;
+
 void help() {
     char* tok = strtok(NULL, " \n");
     if (tok == NULL) {
@@ -19,50 +29,46 @@ void help() {
     }
 }
 
-void scan(int num_vnas, int nbr_scans, int start, int stop, SweepMode sweep_mode, int sweeps, const char **ports) {
+void scan() {
     char* tok = strtok(NULL, " \n");
     if (tok == NULL || (strcmp(tok,"sweeps") == 0)) {
+        sweep_mode = NUM_SWEEPS;
         run_multithreaded_scan(num_vnas, nbr_scans, start, stop, sweep_mode, sweeps, ports);
     }
     else if (strcmp(tok,"time") == 0) {
         sweep_mode = TIME;
         run_multithreaded_scan(num_vnas, nbr_scans, start, stop, sweep_mode, sweeps, ports);
-        sweep_mode = NUM_SWEEPS;
     }
 }
 
-int main() {
-    // set defaults
-    long start = 50000000;
-    long stop = 900000000;
-    int nbr_scans = 20;
-    int sweeps = 5;
-    SweepMode sweep_mode = NUM_SWEEPS;
-    int num_vnas = 1;
-    const char* default_port = "/dev/ttyACM0";
-    const char **ports = (const char **)&default_port;
+int readCommand() {
+    char buff[50];
+    fgets(buff, sizeof(buff), stdin);
 
+    char* tok = strtok(buff, " \n");
 
-    int fin = 0;
-    while (fin != 1) {
-        printf(">>> ");
-        char buff[50];
-        fgets(buff, sizeof(buff), stdin);
-
-        char* tok = strtok(buff, " \n");
-
-        if (tok == NULL) {
-            printf("bad read");
-        }
-        else if (strcmp(tok,"scan") == 0) {
-            scan(num_vnas, nbr_scans, start, stop, sweep_mode, sweeps, ports);
-        }
-        else if (strcmp(tok,"exit") == 0) {
-            fin = 1;
-        }
-        else if (strcmp(tok,"help") == 0) {
-            help();
-        }
+    if (tok == NULL) {
+        printf("bad read");
+    }
+    else if (strcmp(tok,"scan") == 0) {
+        scan();
+    }
+    else if (strcmp(tok,"exit") == 0) {
+        return 1;
+    }
+    else if (strcmp(tok,"help") == 0) {
+        help();
     }
     return 0;
 }
+
+#ifndef TESTSUITE
+int main() {
+    int fin = 0;
+    while (fin != 1) {
+        printf(">>> ");
+        readCommand();
+    }
+    return 0;
+}
+#endif
