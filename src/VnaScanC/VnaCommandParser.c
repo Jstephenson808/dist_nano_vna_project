e59c055c6e087607e54a5f092e1da67c7678eb12
“Keyaan <2904319s@student.gla.ac.uk>
2026-01-20 14:11:32 +0000

    Add 'set' command functionality to modify parameters in VnaCommandParser #96

--- src/VnaScanC/VnaCommandParser.c
+++ src/VnaScanC/VnaCommandParser.c
@@ -18,7 +18,8 @@ void help() {
         printf("\
     scan: starts a scan with current settings\n\
     exit: safely exits the program\n\
-    help: prints a list of all available commands\n"
+    help: prints a list of all available commands\n\
+    set: sets a parameter to a new value\n"
         );
     }
     else if (strcmp(tok,"scan") == 0) {
@@ -27,6 +28,19 @@ void help() {
     scan sweeps - runs a certain number of sweeps (default)  \n\
     scan time - runs sweeps continuosly until specified time elapsed\n"
         );
+    }
+    else if (strcmp(tok,"set") == 0) {
+        printf("\
+    Sets a parameter to a new value.\n\
+    In the terminal, enter: set [parameter] [value]\n\
+    Paramters you can set:\n\
+        start - starting frequency\n\
+        stop - stopping frequency\n\
+        scans - number of scans to compute\n\
+        sweeps - number of sweeps to perform\n\
+        points - number of points per scan\n\
+        vnas - number of VNAs to use\n\
+    For example: set start 100000000\n");
     }
     else {
         printf("Usage: help [command]\nFor list of possible commands type 'help'.\n");
@@ -48,6 +62,60 @@ void scan() {
     }
 }
 
+void set() {
+    char* tok = strtok(NULL, " \n");
+    if (tok == NULL) {
+        printf("Usage: set [parameter] [value]\n");
+        return;
+    }
+    if (strcmp(tok,"start") == 0) {
+        tok = strtok(NULL, " \n");
+        if (tok != NULL) {
+            start = atol(tok);
+            printf("Start frequency set to %ld Hz\n", start);
+        }
+    }
+    else if (strcmp(tok,"stop") == 0) {
+        tok = strtok(NULL, " \n");
+        if (tok != NULL) {
+            stop = atol(tok);
+            printf("Stop frequency set to %ld Hz\n", stop);
+        }
+    }
+    else if (strcmp(tok, "scans") == 0) {
+        tok = strtok(NULL, " \n");
+        if (tok != NULL) {
+            nbr_scans = atoi(tok);
+            printf("Number of scans set to %d\n", nbr_scans);
+        }
+    }
+    else if (strcmp(tok, "sweeps") == 0) {
+        tok = strtok(NULL, " \n");
+        if (tok != NULL) {
+            sweeps = atoi(tok);
+            printf("Number of sweeps set to %d\n", sweeps);
+        }
+    }
+    else if (strcmp(tok, "points") == 0) {
+        tok = strtok(NULL, " \n");
+        if (tok != NULL) {
+            pps = atoi(tok);
+            printf("Points per scan set to %d\n", pps);
+        }
+    }
+    else if (strcmp(tok, "vnas") == 0) {
+        tok = strtok(NULL, " \n");
+        if (tok != NULL) {
+            num_vnas = atoi(tok);
+            printf("Number of VNAs set to %d\n", num_vnas);
+        }
+    }
+    else {
+        printf("Parameter not recognised. Available parameters: start, stop\n");
+    }
+}
+
+
 int readCommand() {
     char buff[50];
     fgets(buff, sizeof(buff), stdin);
@@ -65,6 +133,9 @@ int readCommand() {
     }
     else if (strcmp(tok,"help") == 0) {
         help();
+    }
+    else if (strcmp(tok,"set") == 0) {
+        set();
     }
     else {
         printf("Command not recognised. Type 'help' for list of available commands.\n");
